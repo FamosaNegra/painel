bc39cff06ea4e38f970de883f5e959d5b1ef7d1e
FamosaNegra <114776724+FamosaNegra@users.noreply.github.com>
2025-05-05 15:19:22 -0300

    alterar permissão

--- lib/permissions.tsx
+++ lib/permissions.tsx
@@ -1,7 +1,7 @@
 export const permissions: Record<string, string[]> = {
   OBRAS: ["admin", "cac analyst"],
   ADMIN: ["admin"],
-  USERS: ["admin", "marketing", "cac"],
+  USERS: ["admin", "marketing", "cac", "cac analyst"],
 };
 
 export type PermissionKey = keyof typeof permissions;
